5af64378126b3395251bd96e8483bd87bd631a55
AlfieMcPhee <mcphee.dev@proton.me>
2026-06-17 15:33:27 +0100

    fixed spacing

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         System.out.println("Today I went to a " + adjective1 +" zoo.");
         System.out.println("In an exhibit, I saw a " + noun1 + ".");
-        System.out.println("The" + noun1 + " was " + adjective2 + "  and" + verb1 + " !");
+        System.out.println("The" + noun1 + " was " + adjective2 + "  and " + verb1 + " !");
         System.out.println("I was " + adjective3 +"!");
 
 scanner.close(); // Good practice
