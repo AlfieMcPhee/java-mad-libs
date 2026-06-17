615a41aaa290efe7cf5f031e640dd468cd03d72f
AlfieMcPhee <mcpheeDev@proton.me>
2026-06-17 14:06:27 +0100

    Added basic functionality of the game

--- src/Main.java
+++ src/Main.java
@@ -13,23 +13,21 @@ public class Main {
         String verb1;
         String adjective3;
 
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-        System.out.println();
-
-
-
-
-
-
-
+        System.out.println("Enter an adjective");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective");
+        adjective3 = scanner.nextLine();
 
 
         System.out.println("Today I went to a " + adjective1 +" zoo.");
-        System.out.println("In an exhibit, I saw a " + noun1) + ".";
-        System.out.println(noun1 + " was" + adjective2 + "and" + verb1 + "!");
+        System.out.println("In an exhibit, I saw a " + noun1 + ".");
+        System.out.println("The" noun1 + " was " + adjective2 + "  and" + verb1 + " !");
         System.out.println("I was " + adjective3 +"!");
 
 
